f13f124a8f0c078ca75689638db9c71f8b1b9237
mckadesorensen <dmsorensen@alaska.edu>
2020-11-12 11:55:41 -0900

    Testing

--- cumulus/main.tf
+++ cumulus/main.tf
@@ -149,11 +149,11 @@ resource "aws_s3_bucket_object" "bucket_map_yaml" {
   bucket  = local.system_bucket
   key     = "${local.prefix}-internal/${local.prefix}/thin-egress-app/bucket_map.yaml"
 
-  content = templatefile("${path.module}/thin-egress-app/bucket_map.yaml.tmpl", {
+  content = templatefile("${local.prefix}-internal/${local.prefix}/thin-egress-app/bucket_map.yaml.tmpl", {
     protected_buckets = local.protected_bucket_names,
     public_buckets = local.public_bucket_names
   })
-  etag    = md5(templatefile("${path.module}/thin-egress-app/bucket_map.yaml.tmpl", {
+  etag    = md5(templatefile("${local.prefix}-internal/${local.prefix}/thin-egress-app/bucket_map.yaml.tmpl", {
     protected_buckets = local.protected_bucket_names,
     public_buckets = local.public_bucket_names
   }))
