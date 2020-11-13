e351ea22e6d21be8b4edac3679b2d885c0525a16
mckadesorensen <dmsorensen@alaska.edu>
2020-11-13 11:34:23 -0900

    Uncommented out tea code

--- cumulus/main.tf
+++ cumulus/main.tf
@@ -86,20 +86,20 @@ module "cumulus" {
 
   # Thin Egress App settings
   # must match stack_name variable for thin-egress-app module
-//  tea_stack_name = local.tea_stack_name
-//  # must match stage_name variable for thin-egress-app module
-//  tea_api_gateway_stage = local.tea_stage_name
-//
-//  tea_rest_api_id = module.thin_egress_app.rest_api.id
-//  tea_rest_api_root_resource_id = module.thin_egress_app.rest_api.root_resource_id
-//  tea_internal_api_endpoint = module.thin_egress_app.internal_api_endpoint
-//  tea_external_api_endpoint = module.thin_egress_app.api_endpoint
-//  tea_api_egress_log_group = module.thin_egress_app.egress_log_group
+  tea_stack_name = local.tea_stack_name
+  # must match stage_name variable for thin-egress-app module
+  tea_api_gateway_stage = local.tea_stage_name
+
+  tea_rest_api_id = module.thin_egress_app.rest_api.id
+  tea_rest_api_root_resource_id = module.thin_egress_app.rest_api.root_resource_id
+  tea_internal_api_endpoint = module.thin_egress_app.internal_api_endpoint
+  tea_external_api_endpoint = module.thin_egress_app.api_endpoint
+  tea_api_egress_log_group = module.thin_egress_app.egress_log_group
 }
 
 locals {
   prefix = "${var.DEPLOY_NAME}-cumulus-${var.MATURITY}"
-//  thin_egress_jwt_secret_name = "${local.prefix}-jwt_secret_for_tea"
+  thin_egress_jwt_secret_name = "${local.prefix}-jwt_secret_for_tea"
   daac_remote_state_config = {
     bucket = "${var.DEPLOY_NAME}-cumulus-${var.MATURITY}-tf-state-${substr(data.aws_caller_identity.current.account_id, -4, 4)}"
     key    = "daac/terraform.tfstate"
@@ -145,53 +145,53 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-//resource "aws_s3_bucket_object" "bucket_map_yaml" {
-//  bucket  = local.system_bucket
-//  key     = "${local.prefix}-internal/${local.prefix}/thin-egress-app/bucket_map.yaml"
-//
-//  content = templatefile("${path.module}/thin-egress-app/bucket_map.yaml.tmpl", {
-//    protected_buckets = local.protected_bucket_names,
-//    public_buckets = local.public_bucket_names
-//  })
-//  etag    = md5(templatefile("${path.module}/thin-egress-app/bucket_map.yaml.tmpl", {
-//    protected_buckets = local.protected_bucket_names,
-//    public_buckets = local.public_bucket_names
-//  }))
-//  tags    = local.default_tags
-//}
-//
-//resource "aws_secretsmanager_secret" "thin_egress_urs_creds" {
-//  name_prefix = "${local.prefix}-tea-urs-creds-"
-//  description = "URS credentials for the ${local.prefix} Thin Egress App"
-//  tags        = local.default_tags
-//}
-//
-//resource "aws_secretsmanager_secret_version" "thin_egress_urs_creds" {
-//  secret_id     = aws_secretsmanager_secret.thin_egress_urs_creds.id
-//  secret_string = jsonencode({
-//    UrsId       = var.urs_client_id
-//    UrsAuth     = base64encode("${var.urs_client_id}:${var.urs_client_password}")
-//  })
-//}
+resource "aws_s3_bucket_object" "bucket_map_yaml" {
+  bucket  = local.system_bucket
+  key     = "${local.prefix}-internal/${local.prefix}/thin-egress-app/bucket_map.yaml"
+
+  content = templatefile("${path.module}/thin-egress-app/bucket_map.yaml.tmpl", {
+    protected_buckets = local.protected_bucket_names,
+    public_buckets = local.public_bucket_names
+  })
+  etag    = md5(templatefile("${path.module}/thin-egress-app/bucket_map.yaml.tmpl", {
+    protected_buckets = local.protected_bucket_names,
+    public_buckets = local.public_bucket_names
+  }))
+  tags    = local.default_tags
+}
+
+resource "aws_secretsmanager_secret" "thin_egress_urs_creds" {
+  name_prefix = "${local.prefix}-tea-urs-creds-"
+  description = "URS credentials for the ${local.prefix} Thin Egress App"
+  tags        = local.default_tags
+}
+
+resource "aws_secretsmanager_secret_version" "thin_egress_urs_creds" {
+  secret_id     = aws_secretsmanager_secret.thin_egress_urs_creds.id
+  secret_string = jsonencode({
+    UrsId       = var.urs_client_id
+    UrsAuth     = base64encode("${var.urs_client_id}:${var.urs_client_password}")
+  })
+}
 
 
-//module "thin_egress_app" {
-//  source = "s3::https://s3.amazonaws.com/asf.public.code/thin-egress-app/tea-terraform-build.88.zip"
-//
-//  auth_base_url              = "https://uat.urs.earthdata.nasa.gov"
-//  bucket_map_file            = aws_s3_bucket_object.bucket_map_yaml.id
-//  bucketname_prefix          = ""
-//  config_bucket              = local.system_bucket
-//  domain_name                = var.distribution_url == null ? null : replace(replace(var.distribution_url, "/^https?:///", ""), "//$/", "")
-//  jwt_secret_name            = local.thin_egress_jwt_secret_name
-//  permissions_boundary_name  = var.permissions_boundary_arn == null ? null : reverse(split("/", var.permissions_boundary_arn))[0]
-//  private_vpc                = local.vpc_id
-//  stack_name                 = local.tea_stack_name
-//  stage_name                 = local.tea_stage_name
-//  urs_auth_creds_secret_name = aws_secretsmanager_secret.thin_egress_urs_creds.name
-//  vpc_subnet_ids             = local.lambda_subnet_ids
-//  log_api_gateway_to_cloudwatch  = var.log_api_gateway_to_cloudwatch
-//}
+module "thin_egress_app" {
+  source = "s3::https://s3.amazonaws.com/asf.public.code/thin-egress-app/tea-terraform-build.88.zip"
+
+  auth_base_url              = "https://uat.urs.earthdata.nasa.gov"
+  bucket_map_file            = aws_s3_bucket_object.bucket_map_yaml.id
+  bucketname_prefix          = ""
+  config_bucket              = local.system_bucket
+  domain_name                = var.distribution_url == null ? null : replace(replace(var.distribution_url, "/^https?:///", ""), "//$/", "")
+  jwt_secret_name            = local.thin_egress_jwt_secret_name
+  permissions_boundary_name  = var.permissions_boundary_arn == null ? null : reverse(split("/", var.permissions_boundary_arn))[0]
+  private_vpc                = local.vpc_id
+  stack_name                 = local.tea_stack_name
+  stage_name                 = local.tea_stage_name
+  urs_auth_creds_secret_name = aws_secretsmanager_secret.thin_egress_urs_creds.name
+  vpc_subnet_ids             = local.lambda_subnet_ids
+  log_api_gateway_to_cloudwatch  = var.log_api_gateway_to_cloudwatch
+}
 
 
 
