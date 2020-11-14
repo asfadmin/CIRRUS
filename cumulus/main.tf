9c4a768c06e332433cd10f1f93a23015fef0c5d8
mckadesorensen <dmsorensen@alaska.edu>
2020-11-13 16:06:20 -0900

    Fixed typo

--- cumulus/main.tf
+++ cumulus/main.tf
@@ -180,7 +180,7 @@ module "thin_egress_app" {
 
   bucketname_prefix          = local.prefix  // Will this work?
   config_bucket              = local.system_bucket
-  stack_name                 = local.tea_stage_name
+  stack_name                 = local.tea_stack_name
   urs_auth_creds_secret_name = aws_secretsmanager_secret.thin_egress_urs_creds.name
 }
 
